cf6aa174503acd069c95723e41ed376c269c0181
Tristan <triddel02@gmx.de>
2021-01-23 12:08:50 +0100

    Removed sout statements

--- src/main/java/com/github/tristan_11/gameparameteryaml/model/ValueHandler.java
+++ src/main/java/com/github/tristan_11/gameparameteryaml/model/ValueHandler.java
@@ -33,7 +33,6 @@ public class ValueHandler {
             //to manipulate path variable in second call, so is just get a new one
         }
     }
-    //works
     private void getDescriptionFromMap(Map<String, Object> map, List<String> path, JFXTextArea description) {
         if (map.get(path.get(0)) instanceof Map) {
             map = (Map<String, Object>) map.get(path.get(0));
@@ -46,22 +45,15 @@ public class ValueHandler {
         }
     }
 
-    //broken
     private void getValuesFromMap(Map<String, Object> map, List<String> path, JFXTextField valueField) {
-        System.out.println("[Log] getValuesFromMap ist triggert");
-
         if (map.get(path.get(0)) instanceof Map) {
             map = (Map<String, Object>) map.get(path.get(0));
             if(path.size()>1)path.remove(0);
-            System.out.println("[ValueLog] Im If drin");
             getValuesFromMap(map, path, valueField);
         }
         else {
-            System.out.println("[ValueLog] Im else drin");
             if(map.containsKey(path.get(0)) && !(map.get(path.get(0)) instanceof Map)) {
-                System.out.println("[ValueLog] Im valueSetting drin");
                 valueField.setText(map.get(path.get(0)).toString());
-                System.out.println("[ValueLog] Wert des Textfeldes: " + valueField.getText());
             }
         }
     }
